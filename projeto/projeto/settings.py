0007e6ef01f6978d6f7be44f85ee612fcd74e6a9
LEONARDO PEREIRA DA SILVA <52714788+leopesi@users.noreply.github.com>
2022-03-21 20:57:00 -0300

    Update settings.py

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -134,6 +134,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_DIRS = [
     'statics',
