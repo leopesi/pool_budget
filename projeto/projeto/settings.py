0b5e92d19fa229a8c328648bc00b3937f3196f24
Leonardo <leopesi@yahoo.com.br>
2022-03-21 20:38:04 -0300

    desable graphene_djangot

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'graphene_django',
+    #'graphene_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
